56d4bd474afcb01e37ffb542dcf5027c4234a66a
anthonylavell <anthonylavel@gmail.com>
2025-03-27 17:27:40 -0500

    add code

--- AlgoMunster/AlgoMunster/src/main/java/code/pattern/graph/NumberOfIslands.java
+++ AlgoMunster/AlgoMunster/src/main/java/code/pattern/graph/NumberOfIslands.java
@@ -1,59 +1,63 @@
-package grinder.graph.dfs;
+package code.pattern.graph;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberOfIslands {
-    public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject(getLeetCode());
-        JSONObject leetCode = jsonObject.getJSONObject("data");
-        JSONArray leetCodeGrid =  leetCode.getJSONArray("grid");
-        int rowLen = leetCodeGrid.length();
-        char [][] grid = new char[rowLen][leetCodeGrid.getJSONArray(0).length()];
-        for (int row = 0; row < leetCode.getJSONArray("grid").length(); row++){
-            JSONArray rowVals = leetCodeGrid.getJSONArray(row);
-            for (int col = 0; col < rowVals.length(); col++){
-                grid[row][col] = (char)('0' + rowVals.getInt(col));
+
+    public static class Coordinate {
+        int r;
+        int c;
+
+        public Coordinate(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    private static List<Coordinate> getNeighbors(Coordinate cell, int numRows, int numCols) {
+        List<Coordinate> neighbors = new ArrayList<>();
+        int[] deltaRow = {-1, 0, 1, 0};
+        int[] deltaCol = {0, 1, 0, -1};
+        for (int i = 0; i < deltaRow.length; i++) {
+            int neighborRow = cell.r + deltaRow[i];
+            int neighborCol = cell.c + deltaCol[i];
+            if (0 <= neighborRow && neighborRow < numRows && 0 <= neighborCol && neighborCol < numCols) {
+                neighbors.add(new Coordinate(neighborRow, neighborCol));
             }
         }
-        numIslands(grid);
+        return neighbors;
     }
 
-    public static int numIslands(char[][] grid) {
-        int totalIsLands = 0;
-        for (int row =0; row < grid.length; row++){
-            for (int col = 0; col < grid[row].length; col++){
-                if(grid[row][col] != '0'){
-                    findIslands(grid,row,col);
-                    totalIsLands++;
-                }
+    private static void bfs(List<List<Integer>> grid, Coordinate root, int numRows, int numCols) {
+        ArrayDeque<Coordinate> queue = new ArrayDeque<>();
+        queue.add(root);
+        grid.get(root.r).set(root.c, 0);
+        while (queue.size() > 0) {
+            Coordinate node = queue.pop();
+            List<Coordinate> neighbors = getNeighbors(node, numRows, numCols);
+            for (Coordinate neighbor : neighbors) {
+                if (grid.get(neighbor.r).get(neighbor.c) == 0) continue;
+                queue.add(neighbor);
+                grid.get(neighbor.r).set(neighbor.c, 0);
             }
         }
-        int i =0;
-        return totalIsLands;
     }
-    private static void findIslands(char[][] grid, int sr, int sc){
-        if(sr < 0 || sr >= grid.length || sc < 0 || sc >=grid[sr].length || grid[sr][sc] == '0'){
-            return;
+
+    public static int countNumberOfIslands(List<List<Integer>> grid) {
+        int numRows = grid.size();
+        int numCols = grid.get(0).size();
+        int count = 0;
+        // bfs starting from each unvisited land cell
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numCols; c++) {
+                if (grid.get(r).get(c) == 0) continue;
+                bfs(grid, new Coordinate(r, c), numRows, numCols);
+                // bfs would find 1 connected island, increment count
+                count++;
+            }
         }
-        grid[sr][sc] = '0';
-        findIslands(grid,sr+1,sc);
-        findIslands(grid,sr-1,sc);
-        findIslands(grid,sr,sc+1);
-        findIslands(grid,sr,sc-1);
-
-    }
-
-    private static String getLeetCode(){
-        return """
-                {"data" :{
-                "grid": [
-                          ['1','1','1','1','0'],
-                          ['1','1','0','1','0'],
-                          ['1','1','0','0','0'],
-                          ['0','0','0','0','0']
-                        ]
-                }}
-                """;
+        return count;
     }
 }
--- AlgoMunster/AlgoMunster/src/main/java/code/pattern/graph/dfs/FloodFill.java
+++ AlgoMunster/AlgoMunster/src/main/java/code/pattern/graph/dfs/FloodFill.java
@@ -1,4 +1,99 @@
-package code.pattern.graph.bfs.dfs;
+package code.pattern.graph.dfs;
+/*
+In computer graphics, an uncompressed raster image is presented as a matrix of numbers. Each entry of the matrix represents the color of a pixel. A flood fill algorithm takes a coordinate r, c and a replacement color, and replaces all pixels connected to r, c that have the same color (i.e., the pixels connected to the given coordinate with same color and all the other pixels connected to the those pixels of the same color) with the replacement color. (e.g. MS-Paint's paint bucket tool).
+
+Input
+r: row
+c: column
+replacement: replacement color
+image: an 2D array of integers representing the image
+Output
+the replaced image
+
+Examples
+Example 1:
+Input:
+
+r = 2
+c = 2
+replacement = 9
+arr = [[0,1,3,4,1],[3,8,8,3,3],[6,7,8,8,3],[12,2,8,9,1],[12,3,1,3,2]]
+Output: [[0,1,3,4,1],[3,9,9,3,3],[6,7,9,9,3],[12,2,9,9,1],[12,3,1,3,2]]
+
+Explanation:
+
+From
+
+0 1 3 4 1
+3 8 8 3 3
+6 7 8 8 3
+12 2 8 9 1
+12 3 1 3 2
+to
+
+0 1 3 4 1
+3 9 9 3 3
+6 7 9 9 3
+12 2 9 9 1
+12 3 1 3 2
+ */
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FloodFill {
+
+    public static class Coordinate {
+        int sr;
+        int sc;
+
+        public Coordinate(int r, int c) {
+            this.sr = r;
+            this.sc = c;
+        }
+    }
+
+    public static List<List<Integer>> floodFill(int sr, int sc, int replacement, List<List<Integer>> image) {
+        // WRITE YOUR BRILLIANT CODE HERE
+        int numRows = image.size();
+        int numCols = image.get(0).size();
+        bfs(image, new Coordinate(sr, sc), replacement, numRows, numCols);
+        return image;
+    }
+
+    private static List<Coordinate> getNeighbors(List<List<Integer>> image, Coordinate node, int rootColor, int numRows, int numCols) {
+        List<Coordinate> neighbors = new ArrayList<>();
+        int[] deltaRow = {-1, 0, 1, 0};
+        int[] deltaCol = {0, 1, 0, -1};
+        for (int i = 0; i < deltaRow.length; i++) {
+            int neighborRow = node.sr + deltaRow[i];
+            int neighborCol = node.sc + deltaCol[i];
+            if ( (0 <= neighborRow && neighborRow < numRows) && (0 <= neighborCol && neighborCol < numCols) ){
+                if (image.get(neighborRow).get(neighborCol) == rootColor) {
+                    neighbors.add(new Coordinate(neighborRow, neighborCol));
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    private static void bfs(List<List<Integer>> image, Coordinate root, int replacementColor, int numRows, int numCols) {
+        ArrayDeque<Coordinate> queue = new ArrayDeque<>();
+        queue.add(root);
+        boolean[][] visited = new boolean[numRows][numCols];
+        int rootColor = image.get(root.sr).get(root.sc);
+        image.get(root.sr).set(root.sc, replacementColor); // replace root color
+        visited[root.sr][root.sc] = true;
+        while (queue.size() > 0) {
+            Coordinate node = queue.pop();
+            List<Coordinate> neighbors = getNeighbors(image, node, rootColor, numRows, numCols);
+            for (Coordinate neighbor : neighbors) {
+                if (visited[neighbor.sr][neighbor.sc]) continue;
+                image.get(neighbor.sr).set(neighbor.sc, replacementColor); // replace color
+                queue.add(neighbor);
+                visited[neighbor.sr][neighbor.sc] = true;
+            }
+        }
+    }
 }
