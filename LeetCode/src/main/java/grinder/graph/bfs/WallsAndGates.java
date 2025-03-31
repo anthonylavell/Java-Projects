14337a0bd8720f256a2612bd6e072391ed0fbc98
anthonylavell <anthonylavel@gmail.com>
2025-03-30 20:55:39 -0500

    Refactoring code and add Rotting Oranges Challege

--- LeetCode/src/main/java/grinder/graph/bfs/WallsAndGates.java
+++ LeetCode/src/main/java/grinder/graph/bfs/WallsAndGates.java
@@ -1,7 +1,7 @@
 package grinder.graph.bfs;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 
 public class WallsAndGates {
     public static void main(String[] args) {
@@ -14,30 +14,29 @@ public class WallsAndGates {
     }
 
     public static void wallsAndGates(int[][] rooms) {
-        Deque<int[]> deque = new LinkedList<>();
-        for (int row = 0; row < rooms.length; row++){
-            for (int col = 0; col < rooms[row].length; col++){
-                if(rooms[row][col] == 0){
-                   deque.add(new int[]{row,col});
-                }
+        int[][] dirs = { {-1,0},{1,0},{0,-1},{0,1} };
+        Deque<int[]>deque = new ArrayDeque<>();
+        for(int row = 0; row < rooms.length; row++){
+            for(int col = 0; col < rooms[row].length; col++){
+                if(rooms[row][col] == 0)
+                    deque.add(new int[] {row,col});
             }
         }
-        int [][] dirs = new int[][]{ {-1,0},{1,0},{0,-1},{0,1}};
         for (int level = 1; !deque.isEmpty(); level++){
             int size = deque.size();
             while (size-- > 0){
-                int [] node = deque.pop();
-                for(int []dir : dirs){
-                    int row = dir[0] + node[0];
-                    int col = dir[1] + node[1];
-                    if(row < 0 || row >= rooms.length || col < 0 || col >= rooms[row].length || rooms[row][col] != Integer.MAX_VALUE){
+                int [] nodes = deque.pop();
+                for (int[] dir : dirs) {
+                    int row = nodes[0] + dir[0];
+                    int col = nodes[1] + dir[1];
+                    if (row < 0 || row >= rooms.length || col < 0 || col >= rooms[row].length || rooms[row][col] != Integer.MAX_VALUE) {
                         continue;
                     }
+                    deque.add(new int[]{row, col});
                     rooms[row][col] = level;
-                    deque.add(new int[]{row,col});
                 }
+
             }
         }
     }
-
 }
