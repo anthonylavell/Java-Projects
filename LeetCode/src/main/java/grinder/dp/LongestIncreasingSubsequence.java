cc6a09e4e6eae7af18a94bd61969715225c789e5
anthonylavell <anthonylavel@gmail.com>
2025-04-10 10:57:17 -0500

    Updating files

---
+++ LeetCode/src/main/java/grinder/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,67 @@
+package grinder.dp;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        int [] numbers = {1,3,6,7,9,4,10,5,6};
+        System.out.println(lengthOfLIS(numbers));
+    }
+    public static int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int j = upperBound(dp, nums[i]);
+            if (dp[j - 1] < nums[i] && nums[i] < dp[j]) {
+                dp[j] = nums[i];
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] < Integer.MAX_VALUE) {
+                ans = i;
+            }
+        }
+        return ans;
+    }
+
+    public static int upperBound(int[] dp, int target) {
+        int n = dp.length;
+        int lo = 0, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (dp[mid] > target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    public static int lengthOfLIS2(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int highest = 0;
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            }
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (num > entry.getKey()) {
+                    int temp = entry.getValue() + 1;
+                    if (map.get(num) < temp) {
+                        map.put(num, temp);
+                    }
+                }
+            }
+            highest = Math.max(map.get(num), highest);
+        }
+        return highest;
+    }
+}
---
+++ LeetCode/src/main/java/grinder/dp/MaximalSquare.java
@@ -0,0 +1,32 @@
+package grinder.dp;
+
+public class MaximalSquare {
+    public static void main(String[] args) {
+        char [][] matrix ={{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},
+                {'1','0','0','1','0'}};
+        char [][] matrix2 ={{'0','1'},{'1','0'}};
+        char [][] matrix3 ={{'1'}};
+        System.out.println(maximalSquare(matrix3));
+
+
+    }
+    public static int maximalSquare(char[][] matrix) {
+        int largest = 0;
+        for (int row = 0; row < matrix.length; row++){
+            for (int col = 0; col < matrix[row].length;col++){
+                int subTotal = matrix[row][col]-'0';
+                if(row > 0 && col > 0 && matrix[row][col] != '0'){
+                    int left = matrix[row][col-1]-'0';
+                    int upLeft = matrix[row-1][col-1]-'0';
+                    int up = matrix[row-1][col]-'0';
+                    subTotal+= Math.min(Math.min(left,upLeft),up);
+                }
+               if(subTotal > largest){
+                   largest=subTotal;
+               }
+               matrix[row][col] = (char) (subTotal + '0');
+            }
+        }
+        return largest*largest;
+    }
+}
