1bfab4fff7f76e75ed7688529d349eff43368b23
anthonylavell <anthonylavel@gmail.com>
2025-12-03 09:45:55 -0600

    adding new system design projects

--- LeetCode/src/main/java/arraysandstrings/array/ProductOfArrayExceptSelf.java
+++ LeetCode/src/main/java/arraysandstrings/array/ProductOfArrayExceptSelf.java
@@ -3,25 +3,28 @@ package arraysandstrings.array;
 import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int [] products = new int[nums.length];
+        Arrays.fill(products,1);
+        //prefix
+        int count = 1;
+        for(int i = 0; i < products.length; i++){
+            products[i] = count;
+            count*=nums[i];
+        }
+        //postFix
+        count=1;
+        for(int i = nums.length-1; i >=0; i--){
+            products[i] = products[i] * count;
+            count*=nums[i];
+        }
+        return products;
+
+    }
+
     public static void main(String[] args) {
         int [] arr = {1,2,3,4};
         System.out.println(Arrays.toString(productExceptSelf(arr)));
-    }
-
-    public static int[] productExceptSelf(int[] nums) {
-        int [] totalArray = new int[nums.length];
-        int count = 1;
-        for(int i = 0; i < totalArray.length; i++){
-            totalArray[i] = count;
-            count*=nums[i];
-        }
-        count=1;
-
-        for(int i = nums.length-1; i >=0; i--){
-            totalArray[i] = totalArray[i] * count;
-            count*=nums[i];
-        }
-        return totalArray;
-
     }
 }
--- LeetCode/src/main/java/comp_p/hubspot/TopKFrequentElements.java
+++ LeetCode/src/main/java/comp_p/hubspot/TopKFrequentElements.java
@@ -8,20 +8,27 @@ import java.util.*;
 
 public class TopKFrequentElements {
     public static int[] topKFrequent2(int[] nums, int k) {
-        int[] resultArray = new int[k];
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> b[0]-a[0]);
-        Map<Integer,Integer> mapOfInts = new HashMap<>();
-        for (int num : nums){
-            mapOfInts.put(num,mapOfInts.getOrDefault(num,0)+1);
+        Map<Integer,Integer>mapOfCount = new HashMap<>();
+        List<Integer>[] store = new List[nums.length+1];
+        int [] topFreq = new int[k];
+        for (int num: nums){
+            mapOfCount.putIfAbsent(num,0);
+            mapOfCount.put(num,mapOfCount.get(num)+1);
         }
-        for (Map.Entry<Integer,Integer> entry : mapOfInts.entrySet()){
-            queue.add(new int[]{entry.getValue(),entry.getKey()});
+        for (int index = 0; index < store.length; index++){
+            store[index] =new ArrayList<>();
         }
-        for (int index = 0; index < k && !queue.isEmpty(); index++){
-            int [] tempArray = queue.poll();
-            resultArray[index] = tempArray[1];
+        for (Map.Entry<Integer,Integer> entry : mapOfCount.entrySet()){
+            store[entry.getValue()].add(entry.getKey());
         }
-        return resultArray;
+        int count = 0 ;
+        for (int index = store.length-1; index>=0 && count < k; index--){
+            for (int num : store[index]){
+                topFreq[count]=num;
+                count++;
+            }
+        }
+        return topFreq;
     }
 
     public static void main(String[] args) {
