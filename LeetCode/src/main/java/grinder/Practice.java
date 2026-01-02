98c46d4a64a64fb91aa8569c8571f28d863446d9
anthonylavell <anthonylavel@gmail.com>
2026-01-01 23:15:02 -0600

    add new Classes

--- LeetCode/src/main/java/grinder/Practice.java
+++ LeetCode/src/main/java/grinder/Practice.java
@@ -1,84 +1,49 @@
 package grinder;
 
-class Node{
-    Node[] children;
-    boolean isFound;
-    public Node(){
-        children = new Node[26];
-    }
+import java.util.*;
 
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
 }
 
 public class Practice {
-    private Node root;
-    public Practice() {
-        root = new Node();
-    }
-
-    public void insert(String word) {
-        Node node = root;
-        for (char ch: word.toCharArray()){
-            if (node.children[ch-'a'] == null){
-                node.children[ch-'a'] = new Node();
-            }
-            node = node.children[ch-'a'];
+    public Node cloneGraph(Node node) {
+        if (node==null){
+            return null;
         }
-        node.isFound = true;
-    }
-
-    public boolean search(String word) {
-        Node node = root;
-        for (char ch: word.toCharArray()){
-            node = node.children[ch-'a'];
-            if ( node == null){
-                break;
-            }
-        }
-        return node != null && node.isFound;
-    }
-
-    public boolean startsWith(String prefix) {
-            Node node = root;
-            for (char ch: prefix.toCharArray()){
-                node = node.children[ch-'a'];
-                 if ( node == null){
-                    return false;
+        Map<Node,Node>nodeMap = new HashMap<>();
+        Deque<Node>deque =new ArrayDeque<>();
+        deque.add(node);
+        nodeMap.put(node,new Node(node.val));
+        while (!deque.isEmpty()){
+            Node key = deque.poll();
+            List<Node> neighbors = key.neighbors;
+            for (Node neighbor: neighbors){
+                if (!nodeMap.containsKey(neighbor)){
+                    deque.add(neighbor);
+                    nodeMap.put(neighbor, new Node(neighbor.val));
                 }
+                nodeMap.get(key).neighbors.add(nodeMap.get(neighbor));
             }
-        return true;
+        }
+        return nodeMap.get(node);
     }
 
     public static void main(String[] args) {
         Practice practice = new Practice();
-        practice.insert("app");
-        practice.insert("apple");
-        practice.insert("beer");
-        practice.insert("add");
-        practice.insert("jam");
-        practice.insert("rental");
-
-        System.out.println("Search:");
-        System.out.println(practice.search("apps"));
-        System.out.println(practice.search("app"));
-        System.out.println(practice.search("ad"));
-        System.out.println(practice.search("applepie"));
-        System.out.println(practice.search("rest"));
-        System.out.println(practice.search("jan"));
-        System.out.println(practice.search("rent"));
-        System.out.println(practice.search("beer"));
-        System.out.println(practice.search("jam"));
-
-        System.out.println("StartWith");
-        System.out.println(practice.startsWith("apps"));
-        System.out.println(practice.startsWith("app"));
-        System.out.println(practice.startsWith("ad"));
-        System.out.println(practice.startsWith("applepie"));
-        System.out.println(practice.startsWith("rest"));
-        System.out.println(practice.startsWith("jan"));
-        System.out.println(practice.startsWith("rent"));
-        System.out.println(practice.startsWith("beer"));
-        System.out.println(practice.startsWith("jam"));
-
 
 
     }
