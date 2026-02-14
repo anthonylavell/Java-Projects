84509420680f0be673c5760076a4a1eead4d4df4
anthonylavell <anthonylavel@gmail.com>
2026-02-14 10:24:04 -0600

    Updated Banking application

--- LeetCode/src/main/java/comp_p/capital_one/powerday/bank_account/BankAccount.java
+++ LeetCode/src/main/java/comp_p/capital_one/powerday/bank_account/BankAccount.java
@@ -1,14 +1,13 @@
 package comp_p.capital_one.powerday.bank_account;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BankAccount {
     private Map<String,Account> mapOfAccounts;
@@ -18,62 +17,70 @@ public class BankAccount {
     }
 
     public String sendData(String filePath){
-        if (filePath.endsWith(".json")){
-             jsonData(filePath);
-             return "Success";
-        }else if (filePath.endsWith(".csv")){
-            csvData(filePath);
-            return "Success";
+        String ext = filePath.substring(filePath.lastIndexOf(".")+1);
+        switch (ext){
+            case "csv","txt"->{
+                textData(filePath);
+                return "Success";
+            }
+            case "json"->{
+                jsonData(filePath);
+                return "Success";
+            }
+            default -> {
+                return "File not File";
+            }
         }
-        return "File not File";
+
     }
 
-    public void sendData(List<String> commands){
-        for (String command : commands){
-            String[] commandArr = command.split(",");
-            dataExtraction(commandArr);
+    public void textData(String path){
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            List<String> lines = scanner.useDelimiter("\n")
+                    .tokens()
+                    .filter(token -> !token.isBlank() && !token.startsWith("ts_iso"))
+                    .toList();
+            int i = 0;
+            process(lines);
+        }catch (Exception e){
+            System.out.println("File Not Found");
         }
     }
 
     private void jsonData(String path){
         try {
-            String[] accountTitles = {"ts_iso","type","account_id","account_name",
-                    "currency","amount","source_account_id","destination_account_id","note"};
-            String jsonText = new String (Files.readAllBytes(Paths.get(path)));
+            List<String> accountTitles = List.of("ts_iso","type","account_id","account_name",
+                    "currency","amount","source_account_id","destination_account_id","note");
+            String jsonText = Files.readString(Paths.get(path));
             JSONArray jsonArray = new JSONArray(jsonText);
-            for (int index = 0; index < jsonArray.length(); index++){
+            List<String> lines = IntStream.range(0, jsonArray.length())
+                    .mapToObj(jsonArray::getJSONObject)
+                            .map(jsonObject -> accountTitles.stream()
+                                    .map(key-> jsonObject.optString(key,""))
+                                    .collect(Collectors.joining(","))
+                            )
+                                    .toList();
+            /*for (int index = 0; index < jsonArray.length(); index++){
                 JSONObject object = jsonArray.optJSONObject(index);
-                String[] accountDetails = Stream.of(accountTitles)
-                                .map(k-> object.optString(k,""))
-                                .toArray(String[]::new);
-                dataExtraction(accountDetails);
-
-
-            }
+                String accountDetails = Stream.of(accountTitles)
+                        .map(k-> object.optString(k,""))
+                                .collect(Collectors.joining(","));
+                lines.add(accountDetails);
+                //dataExtraction(accountDetails);
+            }*/
+            process(lines);
         }catch (Exception e){
             System.out.println("File Not Found");
             e.printStackTrace();
         }
 
     }
-    private void csvData(String filePath){
-        try {
-           List<String> commands = new ArrayList<>();
-            File file = new File(filePath);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()){
-                String line = scanner.nextLine().trim();
-                if (line.isEmpty() || line.startsWith("ts_iso,type,account_id")){
-                    continue;
-                }
-
-                String [] parts = line.split(",");
-                dataExtraction(parts);
-            }
-
-        }catch (FileNotFoundException e){
-            System.out.println("File not found.");
-            e.printStackTrace();
+    private void process (List<String>commands){
+         for (String command : commands){
+            String[] commandArr = command.split(",");
+            dataExtraction(commandArr);
         }
     }
 
@@ -89,9 +96,9 @@ public class BankAccount {
             case "deposit" ->{
                 String accountNum = accountDetails[2];
                 double amount = Double.parseDouble(accountDetails[5]);
-                    Account account = isExist(accountNum);
+                Account account = isExist(accountNum);
                 if (account != null && deposit(account, amount)){
-                        System.out.printf("Deposit Of %s, was successfully deposit into %s\n", accountDetails[5], accountDetails[2]);
+                    System.out.printf("Deposit Of %s, was successfully deposit into %s\n", accountDetails[5], accountDetails[2]);
                 }else {
                     System.out.printf("Deposit Of %s, was NOT successfully deposit into %s\n", accountDetails[5], accountDetails[2]);
                 }
@@ -142,10 +149,10 @@ public class BankAccount {
     }
 
     private boolean transfer(Account from, Account to, double amount){
-            if (withDrawn(from,amount)){
-                return false;
-            }
-            deposit(to,amount);
+        if (withDrawn(from,amount)){
+            return false;
+        }
+        deposit(to,amount);
 
         return true;
     }
--- LeetCode/src/main/java/grinder/Practice.java
+++ LeetCode/src/main/java/grinder/Practice.java
@@ -1,43 +1,9 @@
 package grinder;
 
-import java.util.*;
-
 public class Practice {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        Map<Integer, Integer> inDegree = new HashMap<>();
-        Deque<Integer> deque = new ArrayDeque<>();
-        int count = 0;
-        for(int[]pre: prerequisites){
-            inDegree.putIfAbsent(pre[1], 0);
-            inDegree.put(pre[0], inDegree.getOrDefault(pre[0],0) + 1);
-            graph.computeIfAbsent(pre[1],k->new ArrayList<>()).add(pre[0]);
-        }
-
-        for (Map.Entry<Integer, Integer>entry: inDegree.entrySet()){
-            if (entry.getValue() < 1){
-                deque.add(entry.getKey());
-            }
-        }
-
-        while (!deque.isEmpty()){
-            int course = deque.poll();
-            count++;
-            List<Integer> neighbours = graph.getOrDefault(course,new ArrayList<>());
-            for(Integer neighbour: neighbours){
-                inDegree.put(neighbour, inDegree.get(neighbour) - 1);
-                if (inDegree.get(neighbour) == 0){
-                    deque.add(neighbour);
-                }
-            }
-        }
-        return count == inDegree.size();
-    }
 
     public static void main(String[] args) {
         Practice practice = new Practice();
-        int[][] prerequisites = new int[][]{{1,0},{2,1}};
-        int numCourses = 3;
-        System.out.println(practice.canFinish(numCourses, prerequisites));
+
     }
 }
